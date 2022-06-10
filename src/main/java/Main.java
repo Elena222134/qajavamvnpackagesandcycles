cea6ac92683d6360c4fc4b213323f733671cef49
Elena222134 <Vista752@mail.ru>
2022-06-10 15:23:01 +0300

    исправлена сборка. исправлен запуск через mvn

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,10 +3,8 @@ import org.example.qajavamvnPackagesAndCycles.sqr.SQRService;
 public class Main {
     public static void main(String[] args) {
 
-        SQRService service = new SQRService();
-        int sqr = service.calcSqrt(200,300);
-        System.out.println(sqr);
-          }
-      }
+
+    }
+}
 
 
